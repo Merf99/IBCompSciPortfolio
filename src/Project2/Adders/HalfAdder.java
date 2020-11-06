72a04ce99585e9bd0229a2e1298f92fe228aa48b
Merf99 <zoe.s.kr@gmail.com>
2020-11-05 20:38:16 -0600

    Finished half adder

--- src/Project2/Adders/HalfAdder.java
+++ src/Project2/Adders/HalfAdder.java
@@ -1,13 +1,24 @@
 package Project2.Adders;
 
+import Project2.ElementaryLogicGates.And;
+import Project2.ElementaryLogicGates.Xor;
+
 public class HalfAdder {
     public boolean sum;
     public boolean carry;
+    private And and1;
+    private Xor xor1;
 
     public HalfAdder() {
+        and1 = new And();
+        xor1 = new Xor();
     }
 
     public void compute(boolean a, boolean b){
+        and1.compute(a,b);
+        xor1.compute(a,b);
+        carry = and1.out;
+        sum = xor1.out;
 
     }
 
--- src/Project2/Adders/Tests/HalfAdderTest.java
+++ src/Project2/Adders/Tests/HalfAdderTest.java
@@ -8,19 +8,37 @@ import static org.junit.Assert.*;
 public class HalfAdderTest {
 
     @Test
-    public void compute() {
+    public void compute1() {
         HalfAdder halfAdder = new HalfAdder();
         halfAdder.compute(true, true);
         assertEquals(false, halfAdder.sum);
         assertEquals(true, halfAdder.carry);
 
+
+    }
+    @Test
+    public void compute2() {
+        HalfAdder halfAdder = new HalfAdder();
+
         halfAdder.compute(true, false);
         assertEquals(true, halfAdder.sum);
         assertEquals(false, halfAdder.carry);
 
+
+    }
+    @Test
+    public void compute3() {
+        HalfAdder halfAdder = new HalfAdder();
+
         halfAdder.compute(false, true);
         assertEquals(true, halfAdder.sum);
         assertEquals(false, halfAdder.carry);
+
+
+    }
+    @Test
+    public void compute4() {
+        HalfAdder halfAdder = new HalfAdder();
 
         halfAdder.compute(false, false);
         assertEquals(false, halfAdder.sum);
