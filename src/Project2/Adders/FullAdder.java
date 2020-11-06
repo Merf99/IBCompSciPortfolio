cca04f39ea22a2f2182642ab08c3c52efa799492
Merf99 <zoe.s.kr@gmail.com>
2020-11-05 21:07:50 -0600

    Finished half adder and full adder

--- src/Project2/Adders/FullAdder.java
+++ src/Project2/Adders/FullAdder.java
@@ -1,15 +1,35 @@
 package Project2.Adders;
 
+import Project2.ElementaryLogicGates.And;
+import Project2.ElementaryLogicGates.Or;
+import Project2.ElementaryLogicGates.Xor;
+
 public class FullAdder {
 
     public boolean sum;
     public boolean carry;
+    private Xor xor1;
+    private Xor xor2;
+    private And and1;
+    private And and2;
+    private Or or1;
 
     public FullAdder() {
+        xor1 = new Xor();
+        xor2 = new Xor();
+        and1 = new And();
+        and2 = new And();
+        or1 = new Or();
     }
 
     public void compute(boolean a, boolean b, boolean c){
+        xor1.compute(a,b);
+        xor2.compute(xor1.out,c);
+        and1.compute(a,b);
+        and2.compute(xor1.out,c);
+        or1.compute(and1.out,and2.out);
 
-
+        sum = xor2.out;
+        carry= or1.out;
     }
 }
--- src/Project2/Adders/Tests/FullAdderTest.java
+++ src/Project2/Adders/Tests/FullAdderTest.java
@@ -8,40 +8,81 @@ import static org.junit.Assert.*;
 public class FullAdderTest {
 
     @Test
-    public void compute() {
+    public void compute1() {
         FullAdder fullAdder = new FullAdder();
+
         fullAdder.compute(false, false, false);
         assertEquals(false, fullAdder.carry);
         assertEquals(false, fullAdder.sum);
+
+    }
+    @Test
+    public void compute2() {
+        FullAdder fullAdder = new FullAdder();
 
         fullAdder.compute(false, false, true);
         assertEquals(false, fullAdder.carry);
         assertEquals(true, fullAdder.sum);
 
+    }
+    @Test
+    public void compute3() {
+        FullAdder fullAdder = new FullAdder();
+
         fullAdder.compute(false, true, false);
         assertEquals(false, fullAdder.carry);
         assertEquals(true, fullAdder.sum);
+
+
+    }
+    @Test
+    public void compute4() {
+        FullAdder fullAdder = new FullAdder();
 
         fullAdder.compute(false, true, true);
         assertEquals(true, fullAdder.carry);
         assertEquals(false, fullAdder.sum);
 
+
+    }
+    @Test
+    public void compute5() {
+        FullAdder fullAdder = new FullAdder();
+
         fullAdder.compute(true, false, false);
         assertEquals(false, fullAdder.carry);
         assertEquals(true, fullAdder.sum);
+
+
+    }
+    @Test
+    public void compute6() {
+        FullAdder fullAdder = new FullAdder();
 
         fullAdder.compute(true, false, true);
         assertEquals(true, fullAdder.carry);
         assertEquals(false, fullAdder.sum);
 
+
+    }
+    @Test
+    public void compute7() {
+        FullAdder fullAdder = new FullAdder();
+
         fullAdder.compute(true, true, false);
         assertEquals(true, fullAdder.carry);
         assertEquals(false, fullAdder.sum);
 
+
+    }
+    @Test
+    public void compute8() {
+        FullAdder fullAdder = new FullAdder();
+
         fullAdder.compute(true, true, true);
         assertEquals(true, fullAdder.carry);
         assertEquals(true, fullAdder.sum);
-
-
     }
+
+
 }
